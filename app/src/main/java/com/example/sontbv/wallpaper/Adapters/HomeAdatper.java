6f5dc6175fa07bb324acf9e8f9eaf5f8598d63f5
Tran B.V. Son <tranbavinhson@gmail.com>
2017-12-03 17:55:49 +0700

    dm

---
+++ app/src/main/java/com/example/sontbv/wallpaper/Adapters/HomeAdatper.java
@@ -0,0 +1,8 @@
+package com.example.sontbv.wallpaper.Adapters;
+
+/**
+ * Created by sontbv on 12/3/17.
+ */
+
+public class HomeAdatper{
+}
--- app/src/main/java/com/example/sontbv/wallpaper/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/sontbv/wallpaper/Fragments/HomeFragment.java
@@ -3,21 +3,42 @@ package com.example.sontbv.wallpaper.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.sontbv.wallpaper.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Created by sontbv on 12/2/17.
  */
 
 public class HomeFragment extends Fragment {
+    @BindView(R.id.fragment_home_recyclerview)
+    RecyclerView homeRecycerView;
+
+    private Unbinder unbinder;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
         return view;
+    }
+
+
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
